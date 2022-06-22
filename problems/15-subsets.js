51565d258c356f0e148c0abc581a90f524e6d4a2
Maxwell <maxwellchaselawrence@gmail.com>
2022-06-22 12:33:29 -0700

    15

--- problems/15-subsets.js
+++ problems/15-subsets.js
@@ -15,7 +15,42 @@ Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
 ***********************************************************************/
 
 // your code here
+// function subsets(arr){
+// if(arr.length === 0) {
+//   return []
+// }
 
+// let newArr= [[]]
+// for(let i =0; i < arr.length; i++){
+//   let el = arr[i]
+//  newArr.push([el])
+// }
+ 
+//   return newArr.concat([arr], [arr.slice(1, arr.length)], [arr.slice(0, arr.length-1)], [arr.slice(0,1).concat(arr.slice(2,4))])
+
+// }
+function subsets(arr){
+  let res = [];
+
+  let subset = [];
+
+  (function find_subSets(idx) {
+    if (idx >= arr.length) {
+      res.push(subset.slice())
+      return
+    }
+
+    subset.push(arr[idx])
+    find_subSets(idx + 1)
+
+    subset.pop();
+    find_subSets(idx + 1);
+  })(0)
+  // find_subSets(0);
+  return res
+}
+
+console.log(subsets([1, 2, 3])) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 try {
   module.exports = subsets;
